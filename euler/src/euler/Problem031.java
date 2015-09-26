f29ce0e44ab1bc8dc571aa1f8dc265363527c699
Finn Jensen <finnrj@gmail.com>
2015-09-26 18:41:28 +0200

    solved problem 31 but oh so ugly.....

--- euler/src/euler/Problem031.java
+++ euler/src/euler/Problem031.java
@@ -8,24 +8,12 @@ public class Problem031 {
 	final static List<Integer> COINS = Arrays.asList(200, 100, 50, 20, 10, 5, 2,
 			1);
 
-	private static List<Integer> fetch(Integer max, Integer coin) {
+	private static List<Integer> fetchMostPossible(Integer max, Integer coin) {
 		ArrayList<Integer> result = new ArrayList<Integer>();
 		for (int i = 0; i < max / coin; i++) {
 			result.add(coin);
 		}
 		return result;
-	}
-
-	private static Integer largestPossible(List<Integer> coins, Integer value) {
-		return coins.stream().filter(i -> i < value).findFirst().get();
-	}
-
-	private static List<List<Integer>> partition(List<List<Integer>> partitions,
-			Integer maxValue, Integer coinValue) {
-		for (Integer coin : COINS.subList(COINS.indexOf(coinValue), COINS.size())) {
-
-		}
-		return null;
 	}
 
 	public static void main(String[] args) {
@@ -41,7 +29,7 @@ public class Problem031 {
 			for (List<Integer> list : partitions) {
 				int missing = goal - list.stream().reduce(Integer::sum).get();
 				if (missing >= coin && list.get(list.size() - 1) >= coin) {
-					List<Integer> fetched = fetch(missing, coin);
+					List<Integer> fetched = fetchMostPossible(missing, coin);
 					if (coin > 1) {
 						while (!fetched.isEmpty()) {
 							List<Integer> copy = new ArrayList<Integer>(list);
