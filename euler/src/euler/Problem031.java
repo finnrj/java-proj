b34f3f10d79da7b82f7eb5f45e914496d3648cdc
Finn Jensen <finnrj@gmail.com>
2015-09-27 18:28:56 +0200

    solved another one...

--- euler/src/euler/Problem031.java
+++ euler/src/euler/Problem031.java
@@ -2,11 +2,18 @@ package euler;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Problem031 {
-	final static List<Integer> COINS = Arrays.asList(200, 100, 50, 20, 10, 5, 2,
-			1);
+	final static List<Integer> COINS_DESC = Arrays.asList(200, 100, 50, 20, 10,
+			5, 2, 1);
+	final static List<Integer> COINS_ASC = new ArrayList<Integer>(COINS_DESC);
+	static {
+		Collections.reverse(COINS_ASC);
+	}
 
 	private static List<Integer> fetchMostPossible(Integer max, Integer coin) {
 		ArrayList<Integer> result = new ArrayList<Integer>();
@@ -16,15 +23,15 @@ public class Problem031 {
 		return result;
 	}
 
-	public static void main(String[] args) {
-		Integer goal = 200;
+	private static List<List<Integer>> primitiveFinnSolution(List<Integer> coins,
+			Integer goal) {
 		List<List<Integer>> partitions = new ArrayList<List<Integer>>();
-		for (Integer coin : COINS) {
+		for (Integer coin : coins) {
 			if (coin <= goal) {
 				partitions.add(new ArrayList<Integer>(Arrays.asList(coin)));
 			}
 		}
-		for (Integer coin : COINS) {
+		for (Integer coin : coins) {
 			List<List<Integer>> newPartitions = new ArrayList<List<Integer>>();
 			for (List<Integer> list : partitions) {
 				int missing = goal - list.stream().reduce(Integer::sum).get();
@@ -44,7 +51,30 @@ public class Problem031 {
 			}
 			partitions.addAll(newPartitions);
 		}
-		System.out.println(partitions.size());
-		partitions.forEach(System.out::println);
+		return partitions;
 	}
+
+	private static List<Integer> dynamicProgrammingSolutionFromProjectEuler(
+			List<Integer> coins, Integer maxValue) {
+		List<Integer> partitions = Stream.<Integer> iterate(1, i -> 0)
+				.limit(maxValue + 1).collect(Collectors.toList());
+
+		for (Integer coin : coins) {
+			for (int i = coin; i < partitions.size(); i++) {
+				partitions.set(i, partitions.get(i) + partitions.get(i - coin));
+			}
+		}
+		return partitions;
+	}
+
+	public static void main(String[] args) {
+		List<Integer> partitionsCount = dynamicProgrammingSolutionFromProjectEuler(
+				COINS_ASC, 200);
+		System.out.println(partitionsCount.get(partitionsCount.size() - 1));
+
+		List<List<Integer>> partitions = primitiveFinnSolution(COINS_DESC, 200);
+		System.out.println(partitions.size());
+		// partitions.forEach(System.out::println);
+	}
+
 }
--- euler/src/euler/Problem032.java
+++ euler/src/euler/Problem032.java
@@ -8,22 +8,22 @@ import euler.util.Permutations;
 public class Problem032 {
 
 	public static void main(String[] args) {
-		Predicate<? super String> oneTimesFourPandigital = str -> Integer
-				.valueOf(str.substring(0, 1)) * Integer.valueOf(str.substring(1, 5)) == Integer
+		Predicate<String> oneTimesFourPandigital = str -> Integer.valueOf(str
+				.substring(0, 1)) * Integer.valueOf(str.substring(1, 5)) == Integer
 				.valueOf(str.substring(5));
-		Predicate<? super String> twoTimesThreePandigital = str -> Integer
-				.valueOf(str.substring(0, 2)) * Integer.valueOf(str.substring(2, 5)) == Integer
+
+		Predicate<String> twoTimesThreePandigital = str -> Integer.valueOf(str
+				.substring(0, 2)) * Integer.valueOf(str.substring(2, 5)) == Integer
 				.valueOf(str.substring(5));
-		// System.out.println(
+
 		System.out.println(Permutations
 				.of(1, 2, 3, 4, 5, 6, 7, 8, 9)
 				.limit(Permutations.factorial(8) * 7)
 				.map(
 						intStream -> intStream.map(String::valueOf).collect(
 								Collectors.joining()))
-				.filter(
-						((Predicate<String>) oneTimesFourPandigital)
-								.or(twoTimesThreePandigital)).peek(System.out::println)
+				.filter(oneTimesFourPandigital.or(twoTimesThreePandigital))
+				// .peek(System.out::println)
 				.map(str -> str.substring(5)).collect(Collectors.toSet()).stream()
 				.mapToInt(Integer::valueOf).sum());
 		;
