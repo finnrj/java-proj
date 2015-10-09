33105605def17e89fd0a871468abf14c41c0e283
Finn Jensen <finnrj@gmail.com>
2015-10-09 19:27:27 +0200

    solved yet another one...

--- euler/src/euler/Problem047.java
+++ euler/src/euler/Problem047.java
@@ -1,5 +1,7 @@
 package euler;
 
+import java.time.Duration;
+import java.time.Instant;
 import java.util.stream.Collectors;
 import java.util.stream.LongStream;
 
@@ -12,13 +14,43 @@ public class Problem047 {
 				.size() == 4;
 	}
 
+	private static boolean has4PrimeFactors(Long candidate) {
+		int c = 0;
+		for (int p = 2; candidate != 1; p++) {
+			if (candidate % p == 0) {
+				c++;
+				while (candidate % p == 0)
+					candidate /= p;
+			}
+		}
+		return c == 4;
+	}
+
 	public static void main(String[] args) {
-		// 57493
+		// 134043
+		Instant start = Instant.now();
+		// slowOne(start);
+		// start = Instant.now();
+		fastOne(start);
+	}
+
+	private static void fastOne(Instant start) {
 		System.out.println(LongStream
-				.iterate(57493, n -> n + 1)
+				.iterate(84333, n -> n + 1)
+				.peek(System.out::println)
+				.filter(
+						n -> LongStream.rangeClosed(n, n + 3).allMatch(
+								Problem047::has4PrimeFactors)).findFirst());
+		System.out.println(Duration.between(Instant.now(), start));
+	}
+
+	private static void slowOne(Instant start) {
+		System.out.println(LongStream
+				.iterate(84333, n -> n + 1)
 				.peek(System.out::println)
 				.filter(
 						n -> LongStream.rangeClosed(n, n + 3).allMatch(
 								Problem047::hasFourPrimeFactors)).findFirst());
+		System.out.println(Duration.between(Instant.now(), start));
 	}
 }
