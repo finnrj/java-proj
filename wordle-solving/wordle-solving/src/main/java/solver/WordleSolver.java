2dadadf8945f14813515978854357fc3890f1f1c
finn <finnrj@gmail.com>
2025-05-05 21:47:51 +0200

    starting something

--- wordle-solving/wordle-solving/src/main/java/solver/WordleSolver.java
+++ wordle-solving/wordle-solving/src/main/java/solver/WordleSolver.java
@@ -1,4 +1,30 @@
 package solver;
 
+import java.util.*;
+
 public class WordleSolver {
+    public Short compare(String actual, String target) {
+        short result = 0;
+        for (int i = 0; i < target.length(); i++) {
+            if (target.charAt(i) == actual.charAt(i)) {
+                result += 2 * Math.pow(10, (4-i));
+            } else if (target.indexOf(actual.charAt(i)) != -1) {
+                result += 1 * Math.pow(10, (4-i));
+            }
+        }
+        return result;
+    }
+
+    public Map<Short,List<String>> build(String actual, List<String> words) {
+        HashMap<Short, List<String>> result = new HashMap<>();
+        for (String word : words) {
+            short score = compare(actual, word);
+            if (result.containsKey(score)) {
+                result.get(score).add(word);
+            } else {
+                result.put(score, new ArrayList<>(Collections.singletonList(word)));
+            }
+        }
+        return result;
+    }
 }
--- wordle-solving/wordle-solving/src/test/java/solver/WordleSolverTest.java
+++ wordle-solving/wordle-solving/src/test/java/solver/WordleSolverTest.java
@@ -1,8 +1,10 @@
 package solver;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -10,10 +12,65 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class WordleSolverTest {
 
+    private WordleSolver solver;
+
+    @BeforeEach
+    void setUp() {
+        solver = new WordleSolver();
+    }
+
     @Test
-    public void testBuild() {
-        List<String> words = Stream.of("jetty", "wizzo", "cuppa", "cohoe", "gurks", "squad", "beisa", "shrug", "fossa", "fluyt", "camus", "speed", "mamil", "array", "polio", "barns", "panes", "souts", "limas", "fetch", "queck", "twink", "graze", "crock", "almud")
+    void noMatchAtAll() {
+        String candidate = "rossa";
+        String target = "jetty";
+        assertEquals((short) 0, solver.compare(candidate, target));
+    }
+
+    @Test
+    void oneCorrectPosition() {
+        String candidate = "rossa";
+        String target = "cuppa";
+        assertEquals((short)2, solver.compare(candidate, target));
+    }
+
+    @Test
+    void oneCorrectLetters() {
+        String candidate = "rossa";
+        String target = "array";
+        assertEquals((short)10001, solver.compare(candidate, target)  );
+    }
+
+    @Test
+    void mixedResult() {
+        String candidate = "rossa";
+        String target = "graze";
+        assertEquals((short)1100, solver.compare(candidate, target) );
+    }
+
+    @Test
+    public void testBuild10() {
+        List<String> words = Stream.of(
+                "jetty", "wizzo", "cuppa", "cohoe", "gurks", 
+                        "squad", "beisa", "shrug", "fossa", "fluyt")
                 .collect(Collectors.toList());
-        String actual = "rossa";
+        String candidate = "rossa";  //0,1000,2,2000, 10110, 111, 122, 10110, 2222, 0
+        Map<Short, List<String>> result = solver.build(candidate, words);
+        assertEquals(8, result.size() );
+        assertTrue(result.containsKey((short) 0));
+        assertEquals(2, result.get((short)0).size());
+    }
+
+    @Test
+    public void testBuild25() {
+        List<String> words = Stream.of(
+                "jetty", "wizzo", "cuppa", "cohoe",
+                        "gurks", "squad", "beisa", "shrug",
+                        "fossa", "fluyt", "camus", "speed",
+                        "mamil", "array", "polio", "barns",
+                        "panes", "souts", "limas", "fetch",
+                        "queck", "twink", "graze", "crock", "almud")
+                .collect(Collectors.toList());
+        String candidate = "rossa";
+        Map<Short, List<String>> result = solver.build(candidate, words);
     }
 }
